d240f3bbc96890ea32000e9cc15b41096cdf85bd
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-06-02 10:41:18 +0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '12fee609-7b48-48da-a371-913660c396d1';
+export const MODAL_CLOSE_SEC = 2.5;
